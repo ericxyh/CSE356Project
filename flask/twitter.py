c6671a4f868a378b9bad3d9add31852afe35fe7e
Ubuntu <ubuntu@ansible-receipt.cloud.compas.cs.stonybrook.edu>
2019-10-19 06:06:23 +0000

    flask structure

--- flask/twitter.py
+++ flask/twitter.py
@@ -11,3 +11,35 @@ twiu = db.users
 
 app = Flask(__name__)
 mail = Mail(app)
+
+
+@app.route('/adduser', methods = ['GET','POST'])
+def addUser():
+	pass
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+	pass
+@app.route('/logout', methods = ['GET','POST'])
+def logout():
+	pass
+
+@app.route('/verify', methods = ['GET','POST'])
+def verify():
+	pass
+
+@app.route('/additem', methods = ['GET','POST'])
+def addItem():
+	pass
+
+@app.route('/item/<id>', methods = ['GET'])
+def getPost(id):
+	pass
+
+@app.route('/search', methods = ['GET','POST'])
+def search():
+	pass
+
+if __name__ == "__main__":
+#	app.debug = True
+	app.run(host='0.0.0.0',port=80)
